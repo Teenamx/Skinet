ab44bb11fb5f7565bfa20b287b894343b20ea69e
Teenamx <teenamx777@gmail.com>
2022-07-12 13:07:23 +0530

    Designing shop page,adding filter,sort,pagination and search functionality

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+
 import { Component, OnInit } from '@angular/core';
-import { IProduct } from './models/product';
-import { IPagination } from './models/pagination';
+import { IProduct } from './shared/models/product';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,16 +11,10 @@ export class AppComponent implements OnInit {
 
   title = 'Skinet';
   products:IProduct[];
-  constructor(private http:HttpClient){}
+  constructor(){}
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=13').subscribe((response:IPagination)=>{
-      this.products=response.data;
-      console.log(response);
-       },
-      error=>{
-        console.log(error);
-     } );
+
   }
 
 }
